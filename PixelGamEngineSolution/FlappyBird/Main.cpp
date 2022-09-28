0175b28aa59a29fa3f458d9401b508d502d364f8
Sergio6040 <sancho6040@hotmail.com>
2022-09-28 17:08:37 -0500

    ipdate Flappy bird
    +add rectangles, and funtionality
    -needs collisions
    
    -------
    testing sprites at project 1

--- PixelGamEngineSolution/FlappyBird/Main.cpp
+++ PixelGamEngineSolution/FlappyBird/Main.cpp
@@ -11,6 +11,18 @@ private:
 
 	float Gravity = 100.0f;
 
+	float SectionWidth;
+	std::list<int> ListSection;
+	float LevelPosition = 0.0f;
+
+	bool bHasCollided = false;
+
+	bool bResetGame = false;
+
+	int MaxFlapCount = 0;
+
+	int FlapsCount = 0;
+	int AttemptsCount = 0;
 
 public:
 	FFlappyBird()
@@ -21,7 +33,8 @@ public:
 protected:
 	bool OnUserCreate() override
 	{
-		// Called once at the start, so create things here
+		ListSection = { 0, 0, 0, 0 };
+		SectionWidth = (float)ScreenWidth() / (float)(ListSection.size() - 1);
 		return true;
 	}
 
@@ -29,38 +42,104 @@ protected:
 	{
 		// called once per frame
 
-
-		if (GetKey(olc::SPACE).bPressed)
+		if (bResetGame)
 		{
-			BirdAcceleration_y = 0.0f;
-			BirdVelocity_y = -Gravity / 4.0f;
+			bHasCollided = false;
+			bResetGame = false;
+			ListSection = { 0, 0, 0, 0 };
+			BirdAcceleration_y = 0;
+			BirdVelocity_y = 0;
+			BirdPosition_y = ScreenHeight() / 2.0f;
+			FlapsCount = 0;
+			AttemptsCount++;
+		}
+
+
+		if (bHasCollided)
+		{
+			if (GetKey(olc::SPACE).bReleased)
+			{
+				bResetGame = true;
+			}
 		}
 		else
 		{
-			BirdAcceleration_y += Gravity * ElapsedTime;
-		}
 
-		if (BirdAcceleration_y >= Gravity)
-		{
-			BirdAcceleration_y = Gravity;
-		}
 
-		//physics
-		BirdVelocity_y += BirdAcceleration_y * ElapsedTime;
-		BirdPosition_y += BirdVelocity_y * ElapsedTime;
 
-		int Bird_X = (int)(ScreenWidth() / 3.0f);//Sets the x position of the bird, this never changes
+			Clear(olc::BLACK);
 
-		//Draw bird
-		if (BirdVelocity_y > 0)
-		{
-			Draw(Bird_X, BirdPosition_y + 0, olc::WHITE);
-			Draw(Bird_X, BirdPosition_y + 1, olc::BLUE);
-		}
-		else
-		{
-			Draw(Bird_X, BirdPosition_y + 0, olc::BLUE);
-			Draw(Bird_X, BirdPosition_y + 1, olc::WHITE);
+			if (GetKey(olc::SPACE).bPressed && BirdVelocity_y >= Gravity / 2.0f)
+			{
+				BirdAcceleration_y = 0.0f;
+				BirdVelocity_y = -Gravity / 2.0f;
+				FlapsCount++;
+				if (FlapsCount > MaxFlapCount)
+				{
+					MaxFlapCount = FlapsCount;
+				}
+			}
+			else
+			{
+				BirdAcceleration_y += Gravity * ElapsedTime;
+			}
+
+			if (BirdAcceleration_y >= Gravity)
+			{
+				BirdAcceleration_y = Gravity;
+			}
+
+			//physics
+			BirdVelocity_y += BirdAcceleration_y * ElapsedTime;
+			BirdPosition_y += BirdVelocity_y * ElapsedTime;
+
+			LevelPosition += 14.0f * ElapsedTime;
+
+			if (LevelPosition > SectionWidth)
+			{
+				LevelPosition -= SectionWidth;
+				ListSection.pop_front();
+				int i = rand() % (ScreenHeight() - 20);
+
+				if (i <= 10)
+				{
+					i = 0;
+				}
+				ListSection.push_back(i);
+			}
+
+			int SectionIndex = 0;
+			for (auto s : ListSection)
+			{
+				if (s != 0)
+				{
+					DrawRect(SectionIndex * SectionWidth + 10 - LevelPosition, ScreenHeight() - s,
+						SectionWidth / 3, ScreenHeight(),
+						olc::GREEN);
+
+					DrawRect(SectionIndex * SectionWidth + 10 - LevelPosition, 0,
+						SectionWidth / 3, ScreenHeight() - s - 15,
+						olc::GREEN);
+				}
+				SectionIndex++;
+			}
+
+
+			int Bird_X = (int)(ScreenWidth() / 4.0f);//Sets the x position of the bird, this never changes
+
+			//Draw bird
+			if (BirdVelocity_y > 0)
+			{
+				DrawString(Bird_X, BirdPosition_y + 0, " ////");
+				DrawString(Bird_X, BirdPosition_y + 8, "</////O>");
+			}
+			else
+			{
+				DrawString(Bird_X, BirdPosition_y + 0, "</////O>");
+				DrawString(Bird_X, BirdPosition_y + 8, " ////");
+			}
+
+			DrawString(1, 1, "| Attemps: " + std::to_string(AttemptsCount) + "| Flaps: " + std::to_string(FlapsCount) + "| Max Score:" + std::to_string(MaxFlapCount));
 		}
 
 		return true;
@@ -70,7 +149,7 @@ protected:
 int main()
 {
 	FFlappyBird Game;
-	Game.Construct(80, 48, 16, 16);
+	Game.Construct(600, 400, 2, 2);
 	Game.Start();
 
 }
--- PixelGamEngineSolution/Project1/Source.cpp
+++ PixelGamEngineSolution/Project1/Source.cpp
@@ -8,20 +8,32 @@ public:
 	{
 		sAppName = "Example";
 	}
+	olc::Sprite* sDemo = nullptr;
+	olc::Decal* dec = nullptr;
 
 public:
 	bool OnUserCreate() override
 	{
 		// Called once at the start, so create things here
+
+		sDemo = new olc::Sprite("./sprites/redbird-downflap.png");
+		dec = new olc::Decal(sDemo);
+
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
+		/*for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));*/
+
+		Clear(olc::VERY_DARK_BLUE);
+
+		olc::vf2d mouse = { float(GetMouseX()), float(GetMouseY()) };
+
+		DrawSprite(mouse, sDemo);
 		return true;
 	}
 };
