3b747337791224e9c24f6afeb0606257ac627c36
sancho6040 <qwetito@gmail.com>
2022-10-06 15:01:54 -0500

    Create Pathfinding

---
+++ PixelGamEngineSolution/Pathfinding/Main.cpp
@@ -0,0 +1,38 @@
+#define OLC_PGE_APPLICATION
+#include "olcPixelGameEngine.h"
+
+class Example : public olc::PixelGameEngine
+{
+public:
+	Example()
+	{
+		sAppName = "Example";
+	}
+
+public:
+	bool OnUserCreate() override
+	{
+		return true;
+	}
+
+	bool OnUserUpdate(float fElapsedTime) override
+	{
+		// called once per frame
+		for (int x = 0; x < ScreenWidth(); x++)
+			for (int y = 0; y < ScreenHeight(); y++)
+				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+		return true;
+	}
+};
+
+
+int main()
+{
+	Example Game;
+	if (Game.Construct(40, 60, 16, 16))
+	{
+		Game.Start();
+	}
+
+	return 0;
+}
