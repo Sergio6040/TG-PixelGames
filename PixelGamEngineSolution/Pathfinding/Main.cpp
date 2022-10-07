0d8801a4de33bb24d2f5d5f5823c61e1c4185c74
sancho6040 <qwetito@gmail.com>
2022-10-06 19:21:19 -0500

    creating boxes

--- PixelGamEngineSolution/Pathfinding/Main.cpp
+++ PixelGamEngineSolution/Pathfinding/Main.cpp
@@ -1,26 +1,66 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
-class Example : public olc::PixelGameEngine
+class FPathFinding : public olc::PixelGameEngine
 {
 public:
-	Example()
+	FPathFinding()
 	{
-		sAppName = "Example";
+		sAppName = "Pathfinding";
 	}
 
-public:
+private:
+	struct FNode
+	{
+		bool bObstacle = false;
+		bool bVisited = false;
+		float GlobalGoal;
+		float LocalGoal;
+		int X;
+		int Y;
+		std::vector<FNode*> VectorNeighbours;
+		FNode* Parent;
+	};
+
+	FNode* Nodes = nullptr;
+	int MapWidth = 16;
+	int MapHeight = 16;
+
+protected:
 	bool OnUserCreate() override
 	{
+		Nodes = new FNode[MapWidth * MapHeight];
+		for (int i = 0; i < MapWidth; i++)
+		{
+			for (int j = 0; j < MapHeight; j++)
+			{
+				Nodes[j * MapWidth + i].X = i;
+				Nodes[j * MapWidth + i].Y = j;
+				Nodes[j * MapWidth + i].bObstacle = false;
+				Nodes[j * MapWidth + i].Parent = nullptr;
+				Nodes[j * MapWidth + i].bVisited = false;
+			}
+		}
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
+		float NodeSize = 9.0f;
+		float NodeBorder = 2;
+
+		Clear(olc::BLACK);
+
+		for (int i = 0; i < MapWidth; i++)
+		{
+			for (int j = 0; j < MapHeight; j++)
+			{
+				olc::vf2d Start = { i * NodeSize + NodeBorder, j * NodeSize + NodeBorder };
+				olc::vf2d End = { (i + 1) * NodeSize - NodeBorder, (j + 1) * NodeSize - NodeBorder};
+				FillRect(Start, End, olc::BLUE);
+			}
+		}
+
 		return true;
 	}
 };
@@ -28,8 +68,8 @@ public:
 
 int main()
 {
-	Example Game;
-	if (Game.Construct(40, 60, 16, 16))
+	FPathFinding Game;
+	if (Game.Construct(160, 160, 6, 6))
 	{
 		Game.Start();
 	}
