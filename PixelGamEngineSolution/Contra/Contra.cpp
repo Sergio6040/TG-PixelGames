9bd88efa42c1d2abe3dc4a506aa43f3524f3c3d1
Sergio6040 <sancho6040@hotmail.com>
2022-10-11 17:27:17 -0500

    working on tile map

--- PixelGamEngineSolution/Contra/Contra.cpp
+++ PixelGamEngineSolution/Contra/Contra.cpp
@@ -8,47 +8,124 @@ public:
     {
         sAppName = "Example";
     }
-    olc::Sprite* sDemo = nullptr;
-    olc::Decal* dec = nullptr;
 
-    olc::Sprite* sDemo2 = nullptr;
-    olc::Decal* dec2 = nullptr;
+private:
+
+    olc::Sprite* BackgoundSprite = nullptr;
+    olc::Decal* BackGroundDecal = nullptr;
+    
+    //javidx thing
+    std::wstring Level;
+    int LevelWidth;
+    int LevelHeight;
+
+    float CameraX;
+    float CameraY;
+
+    float PlayerX;
+    float PlayerY;
 
 public:
     virtual bool OnUserCreate() override
     {
-        // Called once at the start, so create things here
+        BackgoundSprite = new olc::Sprite("./Assets/Level.png");
+        BackGroundDecal = new olc::Decal(BackgoundSprite);
 
-        sDemo = new olc::Sprite("./sprites/redbird-downflap.png");
-        dec = new olc::Decal(sDemo);
+        LevelWidth = 108;
+        LevelHeight = 7;
+        Level += L"101010101010101010101010101010101010101001010101010101010101010101010101010101010101011010101010101010101010";
+        Level += L"010101010101010101010101010101010101010100101010101010101010101010101010101010101010101101010101010101010101";
+        Level += L"101010101010101010101010101010101010101001010101010101010101010101010101010101010101011010101010101010101010";
+        Level += L"010101010101010101010101010101010101010100101010101010101010101010101010101010101010101101010101010101010101";
+        Level += L"101010101010101010101010101010101010101001010101010101010101010101010101010101010101011010101010101010101010";
+        Level += L"010101010101010101010101010101010101010100101010101010101010101010101010101010101010101101010101010101010101";
+        Level += L"101010101010101010101010101010101010101001010101010101010101010101010101010101010101011010101010101010101010";
 
-
-        sDemo2 = new olc::Sprite("./sprites/background-day.png");
-        dec2 = new olc::Decal(sDemo2);
-		
 
         return true;
     }
 
     virtual bool OnUserUpdate(float fElapsedTime) override
     {
-        // called once per frame
-        /*for (int x = 0; x < ScreenWidth(); x++)
-            for (int y = 0; y < ScreenHeight(); y++)
-                Draw(x, y, olc::Pixel(rand() % 255, rand() % 255, rand() % 255));*/
 
-        Clear(olc::VERY_DARK_BLUE);
+        Clear(olc::BLACK);
 
-        DrawDecal(olc::vf2d(0, 0), dec2, { 0.15f, 0.15f });
+        auto GetTile = [&](int x, int y)
+        {
+            if(x >= 0 && x < LevelWidth && y >= 0 && y < LevelHeight)
+            {
+                return Level[y * LevelWidth + x];
+            }
+            else
+            {
+                return L' ';
+            }
+        };
 
-        olc::vf2d mouse = { float(GetMouseX()), float(GetMouseY()) };
+        auto SetTile = [&](int x, int y, wchar_t c)
+        {
+            if(x >= 0 && x < LevelWidth && y >= 0 && y < LevelHeight)
+            {
+                Level[y * LevelWidth + x] = c;
+            }
+        };
 
-        /*SetPixelMode(olc::Pixel::ALPHA);
-        DrawSprite(mouse, sDemo);
-        SetPixelMode(olc::Pixel::NORMAL);*/
+        if(GetKey(olc::RIGHT).bPressed)
+        {
+            PlayerX += 1;
+        }
+        if(GetKey(olc::LEFT).bPressed)
+        {
+            PlayerX -= 1;
+        }
 
-        DrawDecal(mouse, dec, {0.2f, 0.2f}, olc::Pixel(255, 255, 0, 64));
 
+        CameraX = PlayerX;
+        CameraY = PlayerY;
+
+        int TileWidth = 32;
+        int TileHeight = 32;
+        int VisibleTilesX = ScreenWidth() / TileWidth;
+        int VisibleTilesY = ScreenHeight() / TileHeight;
+
+        float OffSetX = CameraX - (float)VisibleTilesX / 2.f;
+        float OffSetY = CameraY - (float)VisibleTilesY / 2.f;
+
+        //camera clamp
+        if (OffSetX < 0) OffSetX = 0;
+        if (OffSetY < 0) OffSetY = 0;
+        if (OffSetX > LevelWidth - VisibleTilesX) OffSetX = LevelWidth - VisibleTilesX;
+        if (OffSetY > LevelHeight - VisibleTilesY) VisibleTilesY = LevelHeight - VisibleTilesY;
+
+        //smooth offset
+        float fTileOffsetX = (OffSetX - (int)OffSetX) * TileWidth;
+        float fTileOffsetY = (OffSetY - (int)OffSetY) * TileHeight;
+
+        //Draw visible Tile map
+        for (int x = -1; x < VisibleTilesX + 1; x++)
+        {
+            for (int y = -1; y < VisibleTilesY + 1; y++)
+            {
+                wchar_t TileId = GetTile(x + OffSetX, y + OffSetY);
+                if (TileId == '0')
+                {
+                    FillRect(x * TileWidth - OffSetX, y * TileHeight - OffSetY, 32, 32, olc::GREEN);
+                    // DrawPartialDecal();
+                }
+                else if (TileId == '1')
+                {
+                    FillRect(x * TileWidth - OffSetX, y * TileHeight - OffSetY, 32, 32, olc::RED);
+                }
+                else
+                {
+                    FillRect(x * TileWidth - OffSetX, y * TileHeight - OffSetY, 32, 32);
+                }
+            }
+        }
+
+        DrawDecal({0.f, 0.f}, BackGroundDecal);
+
+        
         return true;
     }
 };
@@ -57,8 +134,10 @@ public:
 int main()
 {
     Example demo;
-    if (demo.Construct(40, 60, 16, 16))
+    if (demo.Construct(256, 224, 4, 4))
+    {
         demo.Start();
+    }
 
     return 0;
 }
